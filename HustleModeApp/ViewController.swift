3c4db923b224ea84a99d7c4d8c5efc88203d31aa
shahs210 <shummus@live.co.uk>
2020-12-22 13:58:51 +0000

    Initial Commit

---
+++ HustleModeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HustleModeApp
+//
+//  Created by Shummus Shah on 22/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
